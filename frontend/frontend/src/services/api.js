9c2567f8faf72983780359cf47afa374ff844488
Aryan <aryanpandey2135@gmail.com>
2026-06-25 18:18:44 +0530

    Update frontend API URL

--- frontend/frontend/src/services/api.js
+++ frontend/frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL:
-        "http://localhost:5000/"
+        "https://library-management-backend-ozvk.onrender.com/"
 });
 
 api.interceptors.request.use(
