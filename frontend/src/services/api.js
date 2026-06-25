93de15c8044763c12b152e5dc850eb66b7c578be
Aryan <aryanpandey2135@gmail.com>
2026-06-25 19:30:57 +0530

    fixing URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL:
-        "https://library-management-backend-ozvk.onrender.com"
+        "https://library-management-backend-ozvk.onrender.com/"
 });
 
 api.interceptors.request.use(
