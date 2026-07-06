bbb1580a133c0a4aeb431120ca1fe87c6dc78d0e
Aryan <aryanpandey2135@gmail.com>
2026-07-06 14:20:32 +0530

    URL setting

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,36 +1,22 @@
 import axios from "axios";
 
 const api = axios.create({
-
-    baseURL:
-        "https://library-management-system-backend-0o4u.onrender.com"
-
+    baseURL: import.meta.env.VITE_API_URL
 });
 
 api.interceptors.request.use(
-
     (config) => {
 
-        const token =
-            localStorage.getItem("token");
+        const token = localStorage.getItem("token");
 
         if (token) {
-
-            config.headers.Authorization =
-                `Bearer ${token}`;
-
+            config.headers.Authorization = `Bearer ${token}`;
         }
 
         return config;
 
     },
-
-    (error) => {
-
-        return Promise.reject(error);
-
-    }
-
+    (error) => Promise.reject(error)
 );
 
 export default api;
