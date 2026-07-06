3c1163976d137cfe3235693c746f18d01be72e63
Aryan <aryanpandey2135@gmail.com>
2026-07-06 14:03:35 +0530

    URL setting

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
 
     baseURL:
-        "http://localhost:5000"
+        "https://library-management-system-backend-0o4u.onrender.com"
 
 });
 
